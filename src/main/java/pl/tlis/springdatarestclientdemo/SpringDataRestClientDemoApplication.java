e9585fe4f23ea66f92c5355525303714ab382a14
tomaszlis <lisu89@gmail.com>
2019-05-26 18:37:02 +0200

    Initial commit

---
+++ src/main/java/pl/tlis/springdatarestclientdemo/SpringDataRestClientDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.tlis.springdatarestclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tlis/springdatarestclientdemo/SpringDataRestClientDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tlis.springdatarestclientdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataRestClientDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
